fdd92fceafa66c4fc05105b0323faa52c793b6e9
shishka <112857538+fitolearn@users.noreply.github.com>
2023-08-10 03:50:04 +0300

    cell.h

--- design/cell.h
+++ design/cell.h
@@ -13,11 +13,15 @@ public:
 
     Value GetValue() const override;
     std::string GetText() const override;
-
+// new
+    std::vector<Position> GetReferencedCells() const override;
+    void CacheInvalidate(); 
+    bool IsCircularDependency(const Impl& impl) const;
 private:
     class Impl;
     class EmptyImpl;
     class TextImpl;
     class FormulaImpl;
     std::unique_ptr<Impl> impl_;
+    SheetInterface& sheet_;
 };
