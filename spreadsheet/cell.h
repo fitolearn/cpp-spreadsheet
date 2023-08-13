a53fee80bdaf317084b796fcbfdde5bc766f8aa8
shishka <112857538+fitolearn@users.noreply.github.com>
2023-08-13 03:07:43 +0300

    Update cell.h

--- spreadsheet/cell.h
+++ spreadsheet/cell.h
@@ -1,36 +1,67 @@
 #pragma once
-
 #include "common.h"
 #include "formula.h"
-
 #include <functional>
 #include <unordered_set>
+#include <stack>
+#include <set>
+#include <optional>
 
 class Sheet;
-
 class Cell : public CellInterface {
 public:
-    Cell(Sheet& sheet);
-    ~Cell();
+    explicit Cell(Sheet& sheet);
+    ~Cell() override;
 
     void Set(std::string text);
     void Clear();
 
-    Value GetValue() const override;
-    std::string GetText() const override;
-    std::vector<Position> GetReferencedCells() const override;
-
-    bool IsReferenced() const;
-
+    [[nodiscard]] Value GetValue() const override;
+    [[nodiscard]] std::string GetText() const override;
+    [[nodiscard]] std::vector<Position> GetReferencedCells() const override;
+    void CacheInvalidate(bool status = false);
 private:
-    class Impl;
-    class EmptyImpl;
-    class TextImpl;
-    class FormulaImpl;
+    class Impl {
+    public:
+        [[nodiscard]] virtual Value GetValue() const = 0;
+        [[nodiscard]] virtual std::string GetText() const = 0;
+        [[nodiscard]] virtual std::vector<Position> GetReferencedCells() const;
+        virtual bool HasCache();
+        virtual void ResetCache();
+        virtual ~Impl() = default;
+    };
+    [[nodiscard]] bool CheckCircularDependencies(const Impl& new_impl) const;
+    class EmptyImpl : public Impl {
+    public:
+        [[nodiscard]] Value GetValue() const override;
+        [[nodiscard]] std::string GetText() const override;
+    };
+
+    class TextImpl : public Impl {
+    public:
+        explicit TextImpl(std::string text);
+        [[nodiscard]] Value GetValue() const override;
+        [[nodiscard]] std::string GetText() const override;
+    private:
+        std::string text_;
+    };
+
+    class FormulaImpl : public Impl {
+    public:
+        explicit FormulaImpl(const std::string& text, SheetInterface& sheet);
+        Value GetValue() const override;
+        std::string GetText() const override;
+        std::vector<Position> GetReferencedCells() const override;
+        bool HasCache() override;
+        void ResetCache() override;
+    private:
+        mutable std::optional<FormulaInterface::Value> cache_;
+        std::unique_ptr<FormulaInterface> formula_;
+        SheetInterface& sheet_;
+    };
 
     std::unique_ptr<Impl> impl_;
-
-    // Добавьте поля и методы для связи с таблицей, проверки циклических 
-    // зависимостей, графа зависимостей и т. д.
-
+    Sheet& sheet_;
+    std::set<Cell*> calculated_cells_;
+    std::set<Cell*> used_cells_;
 };
